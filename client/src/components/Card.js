69637995f92199dabafd3299ba287e7565a58625
Terry Hayes <terrychayes@gmail.com>
2017-03-20 22:46:40 -0400

    Update Card and Recommendation components with dummy data for testing.

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,15 +3,25 @@ import React from 'react';
 
 export default function Card (props) {
   //selection will be passed in from parent component Recommendation
-  const {selection} = props
+  const {selection} = props;
 
   //Logic to pick out best card goes here
-  const bestCard;
+  //const bestCard;
+
+  //Hardcoded example below for testing
+  const bestCard = {"name": "Chase Freedom Unlimited",
+     "categories": {
+     	"gas": 0.0225,
+     	"groceries": 0.0225,
+     	"restaurants": 0.0225,
+     	"travel": 0.0225,
+     	"other": 0.0225
+     }
+  }
 
   return (
     <div id='card-recommendation'>
       <h2>{bestCard.name}</h2>
-      <br></br>
       <br></br>
       <h3>{`${bestCard.categories[selection] * 100}% Back`}</h3>
     </div>
--- client/src/components/Recommendation.js
+++ client/src/components/Recommendation.js
@@ -1,16 +1,20 @@
 import React from 'react';
 //import {connect} from 'react-redux';
+import Card from './Card';
 
 export default function Recommendation (props) {
   //props.params will be defined by the variable route '/:selection'
-  const {selection} = props.params;
+  //const {selection} = props.params;
+
+  //Hardcoded example below for testing
+  const selection = 'restaurants';
 
   //Back button below will need to trigger event to reset selection to empty string
   // and render the category list component
   return (
     <div id='recommendation-container'>
       <div id='selection'>
-        <h3>{selection}</h3>
+        <h3>{selection.charAt(0).toUpperCase() + selection.slice(1)}</h3>
       </div>
       <Card selection={selection}/>
       <button className='back-btn'>Back</button>
