0656a6a219a06122c6f266cf2a6ce008d845330d
Terry Hayes <terrychayes@gmail.com>
2017-03-23 23:16:28 -0400

    add Card component for displaying cards on card-chooser selection screen

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import '../../public/css/main.css'
+import '../../public/css/main.css';
+import * as actions from '../actions';
+import {connect} from 'react-redux';
 
-export default function Card (props) {
+export function Card (props) {
+  const toggleCard = props.toggled ? 'card-toggled' : '';
+  function toggler(){
+    props.dispatch(actions.toggleCard(props.index));
+  };
 
   return (
-    <div className='btn results-btn'>
-      <div className='card-text'>
-        {props.name}<br />
+      <div className={`btn results-btn ${toggleCard}`} onClick={toggler}>
+        <div className='card-text'>
+          {props.name}<br />
+        </div>
       </div>
-    </div>
   )
 }
+
+export default connect()(Card);
