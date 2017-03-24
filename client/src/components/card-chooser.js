77d7ca75f51b66b22d167995040e03fefd46acbc
Terry Hayes <terrychayes@gmail.com>
2017-03-24 01:00:36 -0400

    add submitCards method; mapStateToProps

--- client/src/components/card-chooser.js
+++ client/src/components/card-chooser.js
@@ -4,12 +4,22 @@ import * as actions from '../actions';
 import {connect} from 'react-redux';
 import Card from './card';
 
-
-
 export class CardChooser extends React.Component {
+    constructor(props) {
+      super(props);
+      this.submitCards = this.submitCards.bind(this);
+    }
 
     componentDidMount() {
         this.props.dispatch(actions.fetchCards());
+    }
+
+    submitCards(){
+      let formData = {
+        username: this.props.username,
+        cards: this.props.cards
+      }
+      this.props.dispatch(actions.addUserCards(formData));
     }
 
     render() {
@@ -22,14 +32,17 @@ export class CardChooser extends React.Component {
             <div>
               <h3>Please Select Your Cards</h3>
               {cards}
-              <button className='btn submit-cards'>Submit</button>
+              <button className='btn submit-cards' onClick={this.submitCards}>Submit</button>
             </div>
         )
     }
 }
 
 function mapStateToProps(state, props) {
-    return {cards: state.cards}
+    return {
+      cards: state.cards,
+      username: state.username,
+    }
 }
 
 export default connect(mapStateToProps)(CardChooser);
