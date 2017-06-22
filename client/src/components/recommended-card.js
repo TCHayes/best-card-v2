28d4c78a62ac04ee170f87c9b26310a34d76b214
Terry Hayes <TerryCHayes@gmail.com>
2017-06-22 11:50:19 -0400

    add card images directory; update RecommendedCard to display card images

--- client/src/components/recommended-card.js
+++ client/src/components/recommended-card.js
@@ -2,9 +2,13 @@ import React from 'react';
 import '../../public/css/main.css';
 
 export default function RecommendedCard (props) {
+  const img_file = props.name.toLowerCase().replace(/ /g, "_");
 
   return (
-    <div className='btn results-btn'>
+    <div className='recommended-card-screen'>
+      <div className='card-img-container'>
+        <img src={`/card_images/${img_file}.png`} alt={'Recommended Card'} className='card-img' />
+      </div>
       <div className='card-text'>
         {props.name}<br />
         {`${props.percent * 100}% Back`}
